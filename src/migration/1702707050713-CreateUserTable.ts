ff44a2d93c5c6d4644a5ad07ee51647b0f4a2d73
Benny (Kim Wooseok) <VVSOGI@gmail.com>
2023-12-16 18:22:43 +0900

    add migration files

---
+++ src/migration/1702707050713-CreateUserTable.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateUserTable1702707050713 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        CREATE TABLE "user" (
+        id VARCHAR(255) PRIMARY KEY,
+        email VARCHAR(40) UNIQUE NOT NULL,
+        password VARCHAR NOT NULL
+        );
+  `);
+
+    await queryRunner.query(`
+        CREATE TABLE "board" (
+            id VARCHAR(255) PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description VARCHAR NOT NULL,
+            priority VARCHAR NOT NULL DEFAULT 'LOW',
+            email VARCHAR(255) NOT NULL,
+            userId VARCHAR(255),
+            CONSTRAINT fk_user
+            FOREIGN KEY(userId)
+            REFERENCES "user"(id)
+            ON DELETE CASCADE
+        );
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Board 테이블 삭제
+    await queryRunner.query(`DROP TABLE "board";`);
+
+    // User 테이블 삭제
+    await queryRunner.query(`DROP TABLE "user";`);
+  }
+}
